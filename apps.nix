f2a306bdfe265c701b6c70ac6cd11b8e3178a15d
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2025-07-16 14:17:19 -0400

    Split app configuation to a seporate file

---
+++ apps.nix
@@ -0,0 +1,67 @@
+# /etc/nixos/apps.nix
+#
+# This file contains all settings related to apps
+
+{ config, pkgs, ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Installation dependent
+  users.users.woliver99 = {
+    isNormalUser = true;
+    description = "Oliver Wuthrich-Giroux";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      youtube-music
+    ];
+  };
+
+  # Install Flatpak
+  # Don't forget to add the default repository (per user): flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  services.flatpak.enable = true;
+
+  # Install Firefox (Allows for digital certificates in libreoffice)
+  programs.firefox.enable = true;
+
+  # Install Steam
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install Neovim
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #GUI tools
+    gparted
+    localsend
+    
+    #CLI tools
+    htop
+    
+    # Github
+    git
+    gh
+
+    # Set up GPU passthrough
+    pciutils    
+    lshw
+    glxinfo
+
+    # Neovim clipboard
+    wl-clipboard
+
+    # NvChad
+    unzip
+
+    # GNOME extensions
+    gnomeExtensions.pano
+    gnomeExtensions.appindicator
+    gnomeExtensions.gsconnect
+  ];
+}
--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ./bootloader.nix
     ./network.nix
+    ./apps.nix
     ./gpu.nix
   ];
   
@@ -57,66 +58,6 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # Installation dependent
-  users.users.woliver99 = {
-    isNormalUser = true;
-    description = "Oliver Wuthrich-Giroux";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      youtube-music
-    ];
-  };
-
-  # Install Flatpak
-  # Don't forget to add the default repository (per user): flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  services.flatpak.enable = true;
-
-  # Install Firefox (Allows for digital certificates in libreoffice)
-  programs.firefox.enable = true;
-
-  # Install Steam
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Install Neovim
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #GUI tools
-    gparted
-    localsend
-    
-    #CLI tools
-    htop
-    
-    # Github
-    git
-    gh
-
-    # Set up GPU passthrough
-    pciutils    
-    lshw
-    glxinfo
-
-    # Neovim clipboard
-    wl-clipboard
-
-    # NvChad
-    unzip
-
-    # GNOME extensions
-    gnomeExtensions.pano
-    gnomeExtensions.appindicator
-    gnomeExtensions.gsconnect
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
