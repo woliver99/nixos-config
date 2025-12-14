131f931024370b598ab71c4da4390773438308c6
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2025-12-13 19:40:17 -0500

    Working copyous

--- display_manager/gnome/copyous.nix
+++ display_manager/gnome/copyous.nix
@@ -5,15 +5,17 @@
   glib,
   libgda6,
   gsound,
+  libsoup_3,
+  gobject-introspection,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell-extension-copyous";
-  version = "1.1.3";
+  version = "1.2.0";
 
   src = fetchzip {
     url = "https://github.com/boerdereinar/copyous/releases/download/v${finalAttrs.version}/copyous@boerdereinar.dev.zip";
-    hash = "sha256-8LRj+wYKgMWJZYu0ViahatNEkiscCkPZEoLoitNoGbc=";
+    hash = "sha256-3yE0+F/E1/qrGGO6loaMoCzf5gtT1j964/HdytU0ePM=";
     stripRoot = false;
   };
 
@@ -27,11 +29,28 @@ stdenv.mkDerivation (finalAttrs: {
     runHook postBuild
   '';
 
-  # FIX: GNOME 49 (GIRepository 3.0) requires an instance of the Repository 
-  # to set search paths. The static method 'GIRepository.Repository.prepend_search_path' 
-  # was removed. We use 'dup_default()' to get the handle.
+  # FIX:
+  # 1. We inject the 'prepend_search_path' logic into BOTH extension.js (Background) and prefs.js (Settings Window).
+  # 2. We use '.out' for gobject-introspection to ensure we get the libraries, not the binaries.
+  # 3. We convert static 'import Soup' to dynamic imports to prevent load-time crashes before our patch runs.
   preInstall = ''
-    sed -i '1i import GIRepository from "gi://GIRepository"; const repo = GIRepository.Repository.dup_default(); repo.prepend_search_path("${libgda6}/lib/girepository-1.0"); repo.prepend_search_path("${gsound}/lib/girepository-1.0");' extension.js
+    # Define the setup code block with the correct paths
+    SETUP_CODE='import GIRepository from "gi://GIRepository"; \
+    const repo = GIRepository.Repository.dup_default(); \
+    repo.prepend_search_path("${gobject-introspection.out}/lib/girepository-1.0"); \
+    repo.prepend_search_path("${libsoup_3}/lib/girepository-1.0"); \
+    repo.prepend_search_path("${libgda6}/lib/girepository-1.0"); \
+    repo.prepend_search_path("${gsound}/lib/girepository-1.0");'
+
+    # Inject into extension.js (The main extension)
+    sed -i "1i $SETUP_CODE" extension.js
+
+    # Inject into prefs.js (The settings window)
+    sed -i "1i $SETUP_CODE" prefs.js
+
+    # Convert static Soup imports to dynamic to prevent race conditions during load
+    find . -name "*.js" -exec sed -i "s|import Soup from 'gi://Soup?version=3.0';|const { default: Soup } = await import('gi://Soup?version=3.0');|g" {} +
+    find . -name "*.js" -exec sed -i "s|import Soup from 'gi://Soup';|const { default: Soup } = await import('gi://Soup');|g" {} +
   '';
 
   installPhase = ''
