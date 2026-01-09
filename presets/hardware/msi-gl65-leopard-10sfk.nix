3543d5f15c63165a9843cb6cb5f3ed3e5810b19b
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2026-01-08 19:37:53 -0500

    Updated GPU settings for GL65

--- presets/hardware/msi-gl65-leopard-10sfk.nix
+++ presets/hardware/msi-gl65-leopard-10sfk.nix
@@ -3,45 +3,38 @@
 
 { lib, config, pkgs, ... }:
 
+let
+  # --- HELPER FUNCTIONS (From the Docs) ---
+  # These create a high-priority "fake" package that contains only the modified desktop file.
+  patchDesktop = pkg: appName: from: to: lib.hiPrio (
+    pkgs.runCommand "$patched-desktop-entry-for-${appName}" {} ''
+      ${pkgs.coreutils}/bin/mkdir -p $out/share/applications
+      ${pkgs.gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+    ''
+  );
+  
+  # Function to wrap an app with nvidia-offload
+  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
+in
 {
   # -- Config --
   imports = [
     ../../nixos-hardware/msi/gl65 # Import from nixos-hardware repository
   ];
 
-  hardware.graphics = { # (Note: use 'hardware.opengl' if on NixOS < 24.05)
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      nvidia-vaapi-driver
-      libvdpau-va-gl
-    ];
-  };
-
   # Make everything run on the gpu by default
-  hardware.nvidia.prime = {
-    offload.enable = false;
-    sync.enable = true;
-  };
-
-  # --CONDITIONAL VARIABLES --
-  # These will ONLY exist when Sync mode is enabled.
-  # When you switch to "battery-saver" (where sync is false), these vanish automatically.
-  environment.sessionVariables = lib.mkIf config.hardware.nvidia.prime.sync.enable {
-    # 3D Rendering (Vulkan/OpenGL)
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
-
-    # Video Decoding (Force apps to use the Nvidia drivers)
-    LIBVA_DRIVER_NAME = "nvidia";   # Tells apps "Use the nvidia-vaapi-driver"
-    NVD_BACKEND = "direct";         # Required for nvidia-vaapi-driver
-    MOZ_DISABLE_RDD_SANDBOX = "1";  # Required for Firefox/Brave to access the GPU
-  };
+  #hardware.nvidia.prime.offload.enable = false;
+  
+  environment.systemPackages = lib.mkIf (! (lib.elem "battery-saver" config.system.nixos.tags)) [
+    (GPUOffloadApp pkgs.steam "steam")
+  ];
 
   # Enable battery saver specialisation from nixos-hardware
   hardware.nvidia.primeBatterySaverSpecialisation = true;
   specialisation."battery-saver".configuration = {
-    hardware.nvidia.prime.sync.enable = lib.mkForce false;
+    #hardware.nvidia.prime.sync.enable = lib.mkForce false;
+    #hardware.nvidia.prime.reverseSync.enable = lib.mkForce false;
+    hardware.nvidia.prime.offload.enableOffloadCmd = lib.mkForce false;
 
     # RE-ENABLE SLEEP & SUSPEND
     systemd.targets.sleep.enable = lib.mkForce true;
@@ -59,7 +52,7 @@
     "nvme_core.default_ps_max_latency_us=0" # Fix NVMe SSD timeouts (prevent deep sleep)
     "pcie_aspm=off" # Uses more power but testing if this fixes a crash on boot
   ];
-  
+
   # Disable all sleep and suspend states since it causes many problems with the Nvidia drivers
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
