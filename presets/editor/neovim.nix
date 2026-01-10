9b62de0962b9c3538336239ae4153a1e502e8d1c
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2026-01-10 14:09:55 -0500

    Updated neovim to be fully nix

--- presets/editor/neovim.nix
+++ presets/editor/neovim.nix
@@ -2,13 +2,147 @@
 
 { pkgs, ... }:
 
+let
+  nixvim = import (
+    builtins.fetchGit {
+      url = "https://github.com/nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
+      ref = "nixos-25.11";
+    }
+  );
+in
 {
-  programs.neovim = {
+  imports = [ nixvim.nixosModules.nixvim ];
+
+  programs.nixvim = {
     enable = true;
     defaultEditor = true;
 
-    viAlias = true;
-    vimAlias = true;
+    opts = {
+      number = true;
+      shiftwidth = 2;
+      expandtab = true;
+      smartindent = false;
+    };
+
+    extraPackages = with pkgs; [
+      nixfmt-rfc-style
+    ];
+
+    clipboard.providers.wl-copy.enable = true;
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "=";
+        action = "<cmd>lua require('conform').format({ async = true, lsp_fallback = true })<CR>";
+        options.desc = "Format the whole file";
+      }
+      {
+        mode = "v";
+        key = "c";
+        action = "\"+y"; # Note the \" escaping the quote
+        options.desc = "Copy to system clipboard";
+      }
+      {
+        mode = "v";
+        key = "x";
+        action = "\"+x";
+        options.desc = "Cut to system clipboard";
+      }
+      {
+        mode = "v";
+        key = "d";
+        action = "\"_d";
+        options.desc = "Delete without copying";
+      }
+    ];
+
+    colorschemes.catppuccin = {
+      enable = true;
+      settings.flavour = "macchiato";
+    };
+
+    plugins = {
+      # File Explorer
+      nvim-tree.enable = true;
+
+      # Tab Bar
+      bufferline.enable = true;
+
+      # Status Bar
+      lualine.enable = true;
+
+      # Nerdfont Icons Support
+      web-devicons.enable = true;
+
+      # Automatically detects tabstop/shiftwidth per file
+      sleuth.enable = true;
+
+      # Surround visual selections easily (TODO)
+      # nvim-surround.enable = true;
+
+      # Syntax Highlighting
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+          folding.enable = true;
+        };
+      };
+
+      # Completion
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+
+        settings = {
+          mapping = {
+            # (Tab) to confirm the selection
+            "<Tab>" = "cmp.mapping.confirm({ select = true })";
+
+            # (Shift-Tab) to close the completion menu
+            "<S-Tab>" = "cmp.mapping.close()";
+
+            # (Up and Down arrows) to cycle through tablist
+            "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })";
+            "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })";
+
+            # (Ctrl-Space) Open tab menu
+            "<C-Space>" = "cmp.mapping.complete()";
+          };
+
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+        };
+      };
+
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;
+          rust_analyzer = {
+            enable = true;
+            package = null;
+          };
+        };
+      };
+
+      conform-nvim = {
+        enable = true;
+        settings = {
+          formatters_by_ft = {
+            nix = [ "nixfmt" ];
+            #rust = [ "rustfmt" ];
+          };
+        };
+
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
